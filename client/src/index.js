fd7ba1c972d2ab79da566f7eff12ebce8992250f
sashaspelt <sasha.spelt@gmail.com>
2020-08-16 04:34:15 +1000

    Tutorial completed

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+class App extends React.Component {
+    render() {
+        return (
+            <div classname="App">
+                <h1>Hello react</h1>
+            </div>
+            )
+    }
+}
 ReactDOM.render(
   <React.StrictMode>
     <App />
